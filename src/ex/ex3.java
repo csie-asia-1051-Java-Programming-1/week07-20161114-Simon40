ffd7a6b45e1c11eea69f5f351e8fc6cc95d44c5d
Simon40 <105021060@live.asia.edu.tw>
2016-11-14 16:48:52 +0800

    ex4繳交

---
+++ src/ex/ex3.java
@@ -0,0 +1,16 @@
+package ex;
+/*
+ * Date: 2016/11/14
+ * 請寫一個程式，讓使用者輸入一個整數二維陣列，建立 var2()函式 及std2()可以分別計算二維陣列的變異數與標準差，
+ * 並把結果回傳到主程式印出。
+ * Author: 105021060 陳俊仁
+ */
+import java.util.Scanner;
+public class ex3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
